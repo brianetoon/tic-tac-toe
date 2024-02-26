d93c52267a2590f01e7e4dc8ce000e34998903b3
Brian <brian.elliot.toon@gmail.com>
2024-02-26 14:25:05 -0500

    component files for board & square

---
+++ src/components/Square.jsx
@@ -0,0 +1,10 @@
+export default function Square({ value, onSquareClick }) {
+  return (
+    <button 
+      onClick={onSquareClick}
+      className="square"
+    >
+      {value}
+    </button>
+  );
+}
